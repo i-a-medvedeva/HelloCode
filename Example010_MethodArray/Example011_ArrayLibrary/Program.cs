d5a4d3e3a97ba7745452586c77716135e6e62a03
Irina M <i-a-medvedeva@yandex.ru>
2021-12-01 01:25:22 +0300

    Added IndexOf function to find the element

--- Example010_MethodArray/Example011_ArrayLibrary/Program.cs
+++ Example010_MethodArray/Example011_ArrayLibrary/Program.cs
@@ -20,7 +20,30 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf (int[] collection, int find)
+{
+    int count = collection.Length;
+    int index =0;
+    int position = -1;// чтобы при отсутсвии элемента в массиве выдавало -1, а не 0 (что соответ 1ому элем массива)
+    while(index<count)
+    {
+        if(collection[index]==find)
+        {
+            position=index;
+            break;//чтобы закончить после первого найденного элемента
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int [10];
 
 FillArray(array);
+//array[1]=4;//принудительно на 2ую позицию - индекс=1, поставили значение 4, уже после рандомного заполнения массива
 PrintArray(array);
+
+Console.WriteLine();//чтобы выводил пустую строкуб отделяющую массив от позиции
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
