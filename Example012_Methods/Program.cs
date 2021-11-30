4cc542bf96010097f9cf00f52f98f80e279051b3
Irina M <i-a-medvedeva@yandex.ru>
2021-12-01 02:50:08 +0300

    Added for()

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -38,15 +38,24 @@
 
 string Method4(int count, string text)
 {
-    int i=0;
-    string result = String.Empty; //задано значение пустая строка, можно также ="";
-    while (i<count)
+    // int i=0;
+    // string result = String.Empty; //задано значение пустая строка, можно также ="";
+    // while (i<count)
+    // {
+    //     result=result+text;
+    //     i++;
+    // }
+    // return result;
+    //}
+    //string res=Method4(10, "asdf");//10 раз напечатает текст
+    //Console.WriteLine(res);
+    string result=String.Empty;//тоже что выше, только с использованием циклв for
+    for (int i=0; i<count;i++)
     {
         result=result+text;
-        i++;
     }
     return result;
 }
 
-string res=Method4(10, "asdf");//10 раз напечатает текст
+string res=Method4(10, "z");//10 раз напечатает текст
 Console.WriteLine(res);
