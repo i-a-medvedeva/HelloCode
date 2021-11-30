4d6ca9a609053c68fae3f8aa1f7419ebdca597af
Irina M <i-a-medvedeva@yandex.ru>
2021-12-01 00:07:31 +0300

    Added Example010_MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 12, 31, 4,18, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find =18;
+
+int index = 0;
+while (index<n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;// чтобы находил первое такое число в массиве и заканчивал работу, если надо все элементы найти, 
+        //то просто без данного оператора все будет работать!
+    }
+    
+    index++;//index=index+1;
+}
