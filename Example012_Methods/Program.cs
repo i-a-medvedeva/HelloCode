18c45debdfb0c666f4c159903dcc6fcb1da9ab7e
Irina M <i-a-medvedeva@yandex.ru>
2021-12-02 01:48:35 +0300

    Added Task 1 work with text

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -61,7 +61,7 @@ string res=Method4(10, "z");//10 раз напечатает текст
 Console.WriteLine(res); */
 
 //5. Вывод таблицы умножения на экран
-
+/* 
 for (int i=2; i<=10; i++)
 {
     for (int j=2; j<=10; j++)
@@ -69,4 +69,29 @@ for (int i=2; i<=10; i++)
         Console.WriteLine($"{i}*{j}={i*j}");
     }
     Console.WriteLine(); //выводит пустую строку для разрывов страницы
-}
+} */
+
+
+
+
+//Задача 1. Дан текст. В тексте нужно все пробелы заменить черточками, 
+//маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими"с".
+string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+            +"ежели бы вас послали вместо нашего милого Винценгероде,"
+            +"вы бы взяли приступом согласие прусского короля."
+            +"Вы так красноречивы. Вы дадите мне чаю?";
+
+string Replace (string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for(int i=0; i<lenght;i++)
+    {
+        if(text[i]==oldValue) result = result + $"{newValue}";
+        else result=result+$"{text[i]}";
+    }
+    return result;
+}             
+
+string newText=Replace (text, ' ','|');
+Console.WriteLine(newText);
