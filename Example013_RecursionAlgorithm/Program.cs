6c92a871c0fff6bc32a263a23a76cf007120dc92
Irina M <i-a-medvedeva@yandex.ru>
2021-12-05 21:44:49 +0300

    Added two-dimensional array with strings

---
+++ Example013_RecursionAlgorithm/Program.cs
@@ -0,0 +1,13 @@
+﻿//Двумерные массивы
+
+//Пример 1 двумерный массив с типом данных - строка
+
+string[,] table = new string [2,5];
+table [1,2] = "слово";
+for (int rows=0;rows<2;rows++)
+{
+    for (int columns=0; columns<5;columns++) // будет перебирать индексы от 0 до 4 включительно
+    {
+        Console.WriteLine($"-{table[rows, columns]}-");
+    }
+}
